fbc02844b232c440ad49dd4341a9a109d0eb2ffb
wandererabovee <wandererabovee@gmail.com>
2024-01-05 23:16:41 -0800

    About Me Done

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import '../styles/AboutMe.css'; // Import the CSS file
 
-const Blog = () => {
+const Home = () => {
   return (
-    <div style={{ height: '100vh', backgroundColor: 'lightcoral' }}>
-      <h1>Page 3</h1>
+    <div className="home-container">
+      <h1 className="title">About Me</h1>
+      <p className="subtitle">My name is Mike Smith aka WandererAbove, I am a aspiring
+Frontend enginner, Game developer, and Linux acolyte.
+Discover a world of thoughts and inspiration by 
+exploring my blog – where my ideas come to life!</p>
+      
+      {/* White box with a link to play the latest game */}
+      <div className="game-box">
+        <p>Click here to play <br />to look at my blog</p>
+      </div>
+
+      {/* Arrow symbol */}
+      <div className="arrow">&#62;</div>
+
+      {/* Add more content as needed */}
     </div>
   );
 };
 
-export default Blog;
+export default Home;
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="home-container">
       <h1 className="title">Welcome to<br />Mike's Website</h1>
-      <p className="subtitle">My Personal Library to Showcase My Website and Games</p>
+      <p className="subtitle">My Personal Library to Showcase My Websites and Games</p>
       
       {/* White box with a link to play the latest game */}
       <div className="game-box">
